4dbf0e8b19165c39382f7e82669db460fbab88a2
fmg1925 <fmorgado911@gmail.com>
2025-03-27 14:08:43 -0300

    Lanzamiento

---
+++ Presentacion/Productos/Productos_Ingresar.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace Presentacion.Productos
+{
+    partial class Productos_Ingresar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ValorArriendoPorUnidadTextBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.TotalActualTextBox = new System.Windows.Forms.TextBox();
+            this.CantidadArrendadaTextBox = new System.Windows.Forms.TextBox();
+            this.CantidadInicialTextBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.FechaTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.CantidadActualTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.NombreTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Limpiar = new System.Windows.Forms.Button();
+            this.IngresarButton = new System.Windows.Forms.Button();
+            this.Volver = new System.Windows.Forms.Button();
+            this.Salir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ValorArriendoPorUnidadTextBox
+            // 
+            this.ValorArriendoPorUnidadTextBox.Location = new System.Drawing.Point(307, 430);
+            this.ValorArriendoPorUnidadTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.ValorArriendoPorUnidadTextBox.Name = "ValorArriendoPorUnidadTextBox";
+            this.ValorArriendoPorUnidadTextBox.Size = new System.Drawing.Size(539, 29);
+            this.ValorArriendoPorUnidadTextBox.TabIndex = 7;
+            this.ValorArriendoPorUnidadTextBox.TextChanged += new System.EventHandler(this.ValorArriendoPorUnidadTextBox_TextChanged);
+            this.ValorArriendoPorUnidadTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ValorArriendoPorUnidadTextBox_KeyPress);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(32, 369);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(99, 21);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Total Actual:";
+            // 
+            // TotalActualTextBox
+            // 
+            this.TotalActualTextBox.Location = new System.Drawing.Point(307, 365);
+            this.TotalActualTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.TotalActualTextBox.Name = "TotalActualTextBox";
+            this.TotalActualTextBox.Size = new System.Drawing.Size(539, 29);
+            this.TotalActualTextBox.TabIndex = 6;
+            this.TotalActualTextBox.TextChanged += new System.EventHandler(this.TotalActualTextBox_TextChanged);
+            this.TotalActualTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TotalActualTextBox_KeyPress);
+            // 
+            // CantidadArrendadaTextBox
+            // 
+            this.CantidadArrendadaTextBox.Location = new System.Drawing.Point(307, 296);
+            this.CantidadArrendadaTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.CantidadArrendadaTextBox.Name = "CantidadArrendadaTextBox";
+            this.CantidadArrendadaTextBox.Size = new System.Drawing.Size(539, 29);
+            this.CantidadArrendadaTextBox.TabIndex = 5;
+            this.CantidadArrendadaTextBox.TextChanged += new System.EventHandler(this.CantidadArrendadaTextBox_TextChanged);
+            this.CantidadArrendadaTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CantidadArrendadaTextBox_KeyPress);
+            // 
+            // CantidadInicialTextBox
+            // 
+            this.CantidadInicialTextBox.Location = new System.Drawing.Point(307, 176);
+            this.CantidadInicialTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.CantidadInicialTextBox.Name = "CantidadInicialTextBox";
+            this.CantidadInicialTextBox.Size = new System.Drawing.Size(539, 29);
+            this.CantidadInicialTextBox.TabIndex = 3;
+            this.CantidadInicialTextBox.TextChanged += new System.EventHandler(this.CantidadInicialTextBox_TextChanged);
+            this.CantidadInicialTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CantidadInicialTextBox_KeyPress);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(32, 302);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(160, 21);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Cantidad Arrendada:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(32, 179);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(125, 21);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Cantidad Inicial:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(32, 116);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(185, 21);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Fecha de Incorporación:";
+            // 
+            // FechaTextBox
+            // 
+            this.FechaTextBox.Location = new System.Drawing.Point(307, 113);
+            this.FechaTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.FechaTextBox.Name = "FechaTextBox";
+            this.FechaTextBox.Size = new System.Drawing.Size(539, 29);
+            this.FechaTextBox.TabIndex = 2;
+            this.FechaTextBox.TextChanged += new System.EventHandler(this.FechaTextBox_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 58);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nombre:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.CantidadActualTextBox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.NombreTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.ValorArriendoPorUnidadTextBox);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.TotalActualTextBox);
+            this.groupBox1.Controls.Add(this.CantidadArrendadaTextBox);
+            this.groupBox1.Controls.Add(this.CantidadInicialTextBox);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.FechaTextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(110, 53);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(887, 494);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos";
+            // 
+            // CantidadActualTextBox
+            // 
+            this.CantidadActualTextBox.Location = new System.Drawing.Point(307, 234);
+            this.CantidadActualTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.CantidadActualTextBox.Name = "CantidadActualTextBox";
+            this.CantidadActualTextBox.Size = new System.Drawing.Size(539, 29);
+            this.CantidadActualTextBox.TabIndex = 4;
+            this.CantidadActualTextBox.TextChanged += new System.EventHandler(this.CantidadActualTextBox_TextChanged);
+            this.CantidadActualTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CantidadActualTextBox_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 241);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 21);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Cantidad Actual:";
+            // 
+            // NombreTextBox
+            // 
+            this.NombreTextBox.Location = new System.Drawing.Point(307, 54);
+            this.NombreTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.NombreTextBox.Name = "NombreTextBox";
+            this.NombreTextBox.Size = new System.Drawing.Size(539, 29);
+            this.NombreTextBox.TabIndex = 1;
+            this.NombreTextBox.TextChanged += new System.EventHandler(this.NombreTextBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 433);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 21);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Valor Arriendo Por Unidad:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(411, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 25);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Ingreso Nuevo Producto:";
+            // 
+            // Limpiar
+            // 
+            this.Limpiar.Enabled = false;
+            this.Limpiar.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Limpiar.Location = new System.Drawing.Point(252, 555);
+            this.Limpiar.Margin = new System.Windows.Forms.Padding(4);
+            this.Limpiar.Name = "Limpiar";
+            this.Limpiar.Size = new System.Drawing.Size(123, 57);
+            this.Limpiar.TabIndex = 9;
+            this.Limpiar.Text = "Limpiar";
+            this.Limpiar.UseVisualStyleBackColor = true;
+            this.Limpiar.Click += new System.EventHandler(this.Limpiar_Click);
+            // 
+            // IngresarButton
+            // 
+            this.IngresarButton.Enabled = false;
+            this.IngresarButton.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IngresarButton.Location = new System.Drawing.Point(110, 554);
+            this.IngresarButton.Margin = new System.Windows.Forms.Padding(4);
+            this.IngresarButton.Name = "IngresarButton";
+            this.IngresarButton.Size = new System.Drawing.Size(124, 57);
+            this.IngresarButton.TabIndex = 8;
+            this.IngresarButton.Text = "Ingresar";
+            this.IngresarButton.UseVisualStyleBackColor = true;
+            this.IngresarButton.Click += new System.EventHandler(this.IngresarButton_Click);
+            // 
+            // Volver
+            // 
+            this.Volver.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Volver.Location = new System.Drawing.Point(779, 555);
+            this.Volver.Margin = new System.Windows.Forms.Padding(4);
+            this.Volver.Name = "Volver";
+            this.Volver.Size = new System.Drawing.Size(100, 57);
+            this.Volver.TabIndex = 10;
+            this.Volver.Text = "Volver";
+            this.Volver.UseVisualStyleBackColor = true;
+            this.Volver.Click += new System.EventHandler(this.Volver_Click);
+            // 
+            // Salir
+            // 
+            this.Salir.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Salir.Location = new System.Drawing.Point(897, 555);
+            this.Salir.Margin = new System.Windows.Forms.Padding(4);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(100, 57);
+            this.Salir.TabIndex = 11;
+            this.Salir.Text = "Salir";
+            this.Salir.UseVisualStyleBackColor = true;
+            this.Salir.Click += new System.EventHandler(this.Salir_Click);
+            // 
+            // Productos_Ingresar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1107, 624);
+            this.ControlBox = false;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Limpiar);
+            this.Controls.Add(this.IngresarButton);
+            this.Controls.Add(this.Volver);
+            this.Controls.Add(this.Salir);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Productos_Ingresar";
+            this.Text = "Ingresar Productos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ValorArriendoPorUnidadTextBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox TotalActualTextBox;
+        private System.Windows.Forms.TextBox CantidadArrendadaTextBox;
+        private System.Windows.Forms.TextBox CantidadInicialTextBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox FechaTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Limpiar;
+        private System.Windows.Forms.Button IngresarButton;
+        private System.Windows.Forms.Button Volver;
+        private System.Windows.Forms.Button Salir;
+        private System.Windows.Forms.TextBox NombreTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox CantidadActualTextBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Modelo 3 Capas
+
+Programa para visualizar, ingresar, modificar y eliminar Regiones, Provincias, Comunas, Proveedores y Productos realizado en C# .NET\
+3 Capas: Entidad, Datos y Presentación.
+
+
+Usuario: user\
+Contraseña: admin
